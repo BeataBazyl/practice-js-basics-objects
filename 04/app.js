3d54fc224f50da7eb15f18845bc222fa2b57729a
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-14 16:44:30 +0100

    task 04

--- 04/app.js
+++ 04/app.js
@@ -7,3 +7,25 @@ const user = {
         year: '1985'
     }
 }
+
+
+const today = new Date();
+const month = [today.getMonth() +1].toString();
+const day = today.getDate().toString();
+
+user.birthday = function()     {
+
+    if (user.born.month === month && user.born.day == day) {
+        console.log('Happy Birthday');
+    }
+
+    else {
+        console.log('Have a nice day');
+    }
+}
+
+// console.log(user.born.day, user.born.month);
+// console.log(day, month);
+
+
+user.birthday();
